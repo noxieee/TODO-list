fab75026f01a174880af4bbb6ccd5452af39a6c3
Vojtěch Keder <vojta.keder@gmail.com>
2024-08-12 14:50:56 +0200

    Finished basic structure, made list item to hug longer text.

--- src/components/NewTodoPanel.jsx
+++ src/components/NewTodoPanel.jsx
@@ -4,7 +4,7 @@ import "../css/buttons.css"
 export function NewTodoPanel() {
   return (
     <div className="newTodoPanel">
-      <textarea className="newTodoTextArea" spellCheck="false" maxLength="48" placeholder="Go shopping today..."/>
+      <textarea className="newTodoTextArea" spellCheck="false" maxLength="128" placeholder="Go shopping today..."/>
       <button className="primary">Save</button>
     </div>
   );
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,19 +6,11 @@ import {CompletedTitle} from "./CompletedTitle";
 export function TodoList() {
   return (
       <div className="todoList">
-        <TodoTile title="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"/>
+
         <TodoTile title="1"/>
         <TodoTile title="2"/>
         <CompletedTitle/>
         <TodoTile title="3"/>
-        <TodoTile title="4"/>
-        <TodoTile title="4"/>
-        <TodoTile title="4"/>
-        <TodoTile title="4"/>
-        <TodoTile title="4"/>
-        <TodoTile title="4"/>
-        <TodoTile title="4"/>
-        <TodoTile title="4"/>
         <TodoTile title="4"/>
       </div>
   );
