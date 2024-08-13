3bfa320b01d5d238b33cc39b06d1497b90d434c0
Vojtěch Keder <vojta.keder@gmail.com>
2024-08-13 16:28:16 +0200

    A few UI tweaks. Added example todos. Put back StrictMode, it is the cause of everything rendering twice, normal behaviour according to docs.

--- src/components/NewTodoPanel.jsx
+++ src/components/NewTodoPanel.jsx
@@ -1,11 +1,15 @@
 import "../css/newTodoPanel.css"
 import "../css/buttons.css"
 
-export function NewTodoPanel() {
+export function NewTodoPanel(updateTodos, lastID) {
+  function handleSaveClicked() {
+
+  }
+
   return (
     <div className="newTodoPanel">
       <textarea className="newTodoTextArea" spellCheck="false" maxLength="128" placeholder="Go shopping today..."/>
-      <button className="primary">Save</button>
+      <button className="primary" onChange={handleSaveClicked}>Save</button>
     </div>
   );
 }
--- src/components/TodoListWrapper.jsx
+++ src/components/TodoListWrapper.jsx
@@ -12,14 +12,17 @@ export function TodoListWrapper() {
       <div className="todoListWrapper">
         <Header showCompleted={showCompleted} onShowCompletedChange={setShowCompleted}/>
         <TodoList todos={todos} showCompleted={showCompleted} updateTodos={setTodoChanges}/>
-        <NewTodoPanel/>
+        <NewTodoPanel updateTodos={setTodoChanges}/>
       </div>
   );
 }
 
 const TODOS = [
-  {id: 1, title: "aaa", completed: false},
-  {id: 2, title: "bbb", completed: false},
-  {id: 3, title: "ccc", completed: true},
-  {id: 4, title: "ddd", completed: true}
+  {id: 0, title: "Saving todos inside a json?", completed: false},
+  {id: 1, title: "Button for deleting all completed todos?", completed: false},
+  {id: 2, title: "Implement adding new todos", completed: false},
+  {id: 3, title: "Create a static version of the app", completed: true},
+  {id: 4, title: "Make the app somewhat responsive", completed: true},
+  {id: 5, title: "Implement conditional list rendering", completed: true},
+  {id: 6, title: "Make the tile wrap around long text. This is the long text. As you can see, the tile changes height.", completed: true}
 ];
