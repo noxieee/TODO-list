8e1f3a8c525f4adf3f83097677e01936bc23a474
Vojtěch Keder <vojta.keder@gmail.com>
2024-08-13 12:10:26 +0200

    Todo tile now remembers if it is completed or not.

--- src/components/TodoTile.jsx
+++ src/components/TodoTile.jsx
@@ -1,10 +1,17 @@
 import "../css/todoTile.css"
+import {useState} from "react";
 
 export function TodoTile({title, completed}) {
+  const [isCompleted, setIsCompleted] = useState(completed);
+
+  function checkboxClicked() {
+    setIsCompleted(!isCompleted);
+  }
+
   return (
       <div className="todoTile">
         <p className="todoTitle">{title}</p>
-        <input type="checkbox" checked={completed}/>
+        <input type="checkbox" checked={isCompleted} onChange={checkboxClicked}/>
       </div>
   );
 }
